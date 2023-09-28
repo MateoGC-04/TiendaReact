b929c14fa11c65b81145db9bb6cc94c96d3b7bcd
MateoGC-04 <mateogonzalezcano04@gmail.com>
2023-09-28 03:26:28 -0500

    Header Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Head from "./components/top";
 
-function App() {
+import ShowItems from "./components/itemList";
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Head />
+      <ShowItems />
     </div>
   );
 }
-
-export default App;
